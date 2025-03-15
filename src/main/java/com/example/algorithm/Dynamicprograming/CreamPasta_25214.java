cafd8045f731a43db72b263706ed1d06ff7a7ea0
DK <jjk1491@gmail.com>
2025-03-15 15:02:34 +0900

    크림파스타_25214_25.03.15

---
+++ src/main/java/com/example/algorithm/Dynamicprograming/CreamPasta_25214.java
@@ -0,0 +1,35 @@
+package com.example.algorithm.Dynamicprograming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CreamPasta_25214 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    // 배열의 크기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int minValue = Integer.MAX_VALUE;
+        int maxDiff = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (i > 0) {
+                maxDiff = Math.max(maxDiff, num - minValue);
+                sb.append(maxDiff).append(" ");
+            } else {
+                sb.append(0).append(" ");
+            }
+
+            minValue = Math.min(minValue, num);
+
+        }
+
+        System.out.println(sb.toString().trim());
+
+    }
+}
