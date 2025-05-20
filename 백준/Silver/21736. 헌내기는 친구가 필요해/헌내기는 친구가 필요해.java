07e45c61b2a1676777dd23ed7060b3d0cc48953c
LeeDongKyu <jjk1491@gmail.com>
2025-05-20 10:01:31 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 380 ms, Memory: 36556 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new char[N][M];
+        visited = new boolean[N][M];
+
+        int startX = 0, startY = 0;
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j);
+                if (map[i][j] == 'I') {
+                    startX = i;
+                    startY = j;
+                }
+            }
+        }
+
+        bfs(startX, startY);
+
+        if (count == 0) System.out.println("TT");
+        else System.out.println(count);
+    }
+
+    static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int cx = current[0];
+            int cy = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (visited[nx][ny] || map[nx][ny] == 'X') continue;
+
+                visited[nx][ny] = true;
+                if (map[nx][ny] == 'P') count++;
+                q.offer(new int[]{nx, ny});
+            }
+        }
+    }
+}
