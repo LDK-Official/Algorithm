696998b44e23cc0914feb570629ed0a1710c2f7e
LeeDongKyu <jjk1491@gmail.com>
2025-06-15 14:07:31 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            Map<String, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String type = st.nextToken();
+
+                map.put(type, map.getOrDefault(type, 0) + 1);
+            }
+
+            int result = 1;
+            for (int count : map.values()) {
+                result *= (count + 1);
+            }
+
+            System.out.println(result - 1); 
+        }
+    }
+}
