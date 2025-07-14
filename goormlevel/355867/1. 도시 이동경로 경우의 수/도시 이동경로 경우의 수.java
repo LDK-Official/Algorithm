96009e50899610d9d93ece26b8607f5317e0655e
LeeDongKyu <jjk1491@gmail.com>
2025-07-14 09:18:54 +0900

    [난이도 3] Title: 도시 이동경로 경우의 수, Time: 0.06 ms, Memory: 31.41 MB -BaekjoonHub

---
+++ goormlevel/355867/1. 도시 이동경로 경우의 수/도시 이동경로 경우의 수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class Main {
+    static final int MOD = 1000000007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) {
+            System.out.println(1);
+        } else {
+            System.out.println(pow(2, N - 2, MOD));
+        }
+    }
+    static long pow(long base, long exp, long mod) {
+        long result = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) result = (result * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+}
