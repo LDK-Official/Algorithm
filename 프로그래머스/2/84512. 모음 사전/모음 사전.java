8dd8560f42d41e7c24b40ab0ef37edb22fac3480
LeeDongKyu <jjk1491@gmail.com>
2025-10-03 02:34:39 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String word) {
+        char[] vowels = {'A', 'E', 'I', 'O', 'U'};
+        int[] weight = {781, 156, 31, 6, 1};
+        
+        int answer = 0;
+        
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int idx = 0;
+            for (int j = 0; j < vowels.length; j++) {
+                if (vowels[j] == c) {
+                    idx = j;
+                    break;
+                }
+            }
+            answer += idx * weight[i] + 1;
+        }
+        
+        return answer;
+    }
+}
