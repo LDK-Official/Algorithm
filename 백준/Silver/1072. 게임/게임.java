91d986b6703d502f9f7cda3cc41d635acf277942
LeeDongKyu <jjk1491@gmail.com>
2025-10-13 09:27:53 +0900

    [Silver III] Title: 게임, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long X = Long.parseLong(st.nextToken());
+        long Y = Long.parseLong(st.nextToken());
+        long Z = (Y * 100) / X;
+
+        if (Z >= 99){
+            System.out.println(-1);
+            return;
+        }
+
+        long left = 1;
+        long right = 1000000000;
+        long ans = -1;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long newZ = ((Y + mid) * 100) / (X + mid);
+
+            if (newZ > Z) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
