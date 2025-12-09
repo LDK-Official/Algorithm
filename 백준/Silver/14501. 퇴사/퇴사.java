81a1da711c779b553eeb7ca92cb5f3a2453501f2
LeeDongKyu <jjk1491@gmail.com>
2025-12-09 09:12:42 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine().trim());
+
+        int[] T = new int[N + 2];
+        int[] P = new int[N + 2];
+        int[] dp = new int[N + 3];
+
+        for (int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = N; i >0; i--){
+            int nextDay = i + T[i];
+
+            if (nextDay <= N + 1){
+                dp[i] = Math.max(dp[nextDay] + P[i], dp[i + 1]);
+            }else {
+                dp[i] = dp[i + 1];
+            }
+        }
+
+        System.out.println(dp[1]);
+
+    }
+}
