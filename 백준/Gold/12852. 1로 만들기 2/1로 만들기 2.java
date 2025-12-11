01269fb1734bb66ecce44623f99a222d6aa4fcc9
LeeDongKyu <jjk1491@gmail.com>
2025-12-11 16:22:44 +0900

    [Gold V] Title: 1로 만들기 2, Time: 124 ms, Memory: 21976 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        int[] prev = new int[N + 1];
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+
+            dp[i] = dp[i - 1] + 1;
+            prev[i] = i - 1;
+
+            if (i % 2 == 0) {
+                if (dp[i] > dp[i / 2] + 1) {
+                    dp[i] = dp[i / 2] + 1;
+                    prev[i] = i / 2;
+                }
+            }
+
+            if (i % 3 == 0) {
+                if (dp[i] > dp[i / 3] + 1) {
+                    dp[i] = dp[i / 3] + 1;
+                    prev[i] = i / 3;
+                }
+            }
+        }
+
+        System.out.println(dp[N]);
+
+        StringBuilder sb = new StringBuilder();
+        int cur = N;
+
+        while (cur != 0) {
+            sb.append(cur).append(" ");
+            if (cur == 1) break;
+            cur = prev[cur];
+        }
+
+        System.out.println(sb);
+    }
+}
