fcb06e73342c2a5b3dbab99b390f79bc04531ce5
LeeDongKyu <jjk1491@gmail.com>
2026-03-19 20:49:32 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2388 ms, Memory: 447200 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,83 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int value, id;
-        Node(int value, int id) {
-            this.value = value;
-            this.id = id;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
-
             int k = Integer.parseInt(br.readLine());
-
-            PriorityQueue<Node> maxPQ = new PriorityQueue<>(
-                    (a, b) -> Integer.compare(b.value, a.value)
-            );
-
-            PriorityQueue<Node> minPQ = new PriorityQueue<>(
-                    (a, b) -> Integer.compare(a.value, b.value)
-            );
-
-            boolean[] visited = new boolean[k];
-
-            int id = 0;
+            TreeMap<Integer, Integer> map = new TreeMap<>();
 
             for (int i = 0; i < k; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                String cmd = st.nextToken();
-                int num = Integer.parseInt(st.nextToken());
+                char op = st.nextToken().charAt(0);
+                int n = Integer.parseInt(st.nextToken());
 
-                if (cmd.equals("I")) {
-                    maxPQ.add(new Node(num, id));
-                    minPQ.add(new Node(num, id));
-                    visited[id] = true;
-                    id++;
-
+                if (op == 'I') {
+                    map.put(n, map.getOrDefault(n, 0) + 1);
                 } else {
-                    if (num == 1) {
+                    if (map.isEmpty()) continue;
 
-                        while (!maxPQ.isEmpty() && !visited[maxPQ.peek().id]) {
-                            maxPQ.poll();
-                        }
-                        if (!maxPQ.isEmpty()) {
-                            visited[maxPQ.peek().id] = false;
-                            maxPQ.poll();
-                        }
+                    int targetKey = (n == 1) ? map.lastKey() : map.firstKey();
+                    
+                    if (map.get(targetKey) == 1) {
+                        map.remove(targetKey);
                     } else {
-                        while (!minPQ.isEmpty() && !visited[minPQ.peek().id]) {
-                            minPQ.poll();
-                        }
-                        if (!minPQ.isEmpty()) {
-                            visited[minPQ.peek().id] = false;
-                            minPQ.poll();
-                        }
+                        map.put(targetKey, map.get(targetKey) - 1);
                     }
                 }
             }
 
-            while (!maxPQ.isEmpty() && !visited[maxPQ.peek().id]) {
-                maxPQ.poll();
-            }
-            while (!minPQ.isEmpty() && !visited[minPQ.peek().id]) {
-                minPQ.poll();
-            }
-
-            if (maxPQ.isEmpty()) {
-                sb.append("EMPTY\n");
+            if (map.isEmpty()) {
+                System.out.println("EMPTY");
             } else {
-                sb.append(maxPQ.peek().value).append(" ")
-                  .append(minPQ.peek().value).append("\n");
+                System.out.println(map.lastKey() + " " + map.firstKey());
             }
         }
-
-        System.out.print(sb);
     }
 }
